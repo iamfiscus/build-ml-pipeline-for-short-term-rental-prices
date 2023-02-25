85e58b899d4ea88eb52de6b3a968775f2539dd15
Jd Fiscus <me@iamfiscus.com>
2023-02-25 16:29:08 -0500

    Step 5: release next

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -36,10 +36,10 @@ def go(args):
     idx = df["price"].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
-    # idx = df['longitude'].between(-74.25, -
-    #                               73.50) & df['latitude'].between(40.5, 41.2)
-    # logger.info("copy dataframe by [idx]")
-    # df = df[idx].copy()
+    idx = df['longitude'].between(-74.25, -
+                                  73.50) & df['latitude'].between(40.5, 41.2)
+    logger.info("copy dataframe by [idx]")
+    df = df[idx].copy()
 
     logger.info("last_review converts into datetime")
     df["last_review"] = pd.to_datetime(df["last_review"])
